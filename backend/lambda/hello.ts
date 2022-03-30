63772d55f0fda3ddde6a2e5742c3bc9aa6fe21db
tanimon <8575113+tanimon@users.noreply.github.com>
2022-03-30 19:23:07 +0900

    Update `hello` handler to return a JSON as the body

--- backend/lambda/hello.ts
+++ backend/lambda/hello.ts
@@ -4,6 +4,8 @@ export const handler: APIGatewayProxyHandler = async (_) => {
   return {
     statusCode: 200,
     isBase64Encoded: false,
-    body: 'Hello World! You are successfully authorized with Cognito!',
+    body: JSON.stringify({
+      message: 'Hello World! You are successfully authorized with Cognito!',
+    }),
   };
 };
